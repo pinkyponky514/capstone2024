c0acea666361cf37e446755b736e0333c9f77b31
YuHyunGyung <hghgy@hansung.ac.kr>
2024-05-17 18:44:39 +0900

    merge

--- app/src/main/java/com/example/reservationapp/Retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/reservationapp/Retrofit/RetrofitClient.kt
@@ -1,26 +1,16 @@
 package com.example.reservationapp.Retrofit
 
-import android.app.Application
-import android.content.Context
-import android.content.SharedPreferences
 import com.example.reservationapp.Model.APIService
 import com.google.gson.GsonBuilder
-import kotlinx.coroutines.runBlocking
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Response
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
-import java.util.concurrent.TimeUnit
-import javax.inject.Inject
 
 object RetrofitClient {
 
     private var instance: RetrofitClient? = null
     private var apiService: APIService? = null
-    private lateinit var retrofit: Retrofit
+    private var retrofit: Retrofit
     private const val baseUrl = "http://192.168.35.240:8080"
 
     private val gson = GsonBuilder().setLenient().create()
